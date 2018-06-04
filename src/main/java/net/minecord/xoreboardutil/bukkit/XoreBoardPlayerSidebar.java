c36f542147b5fed59b5b044801683da54d72f411
Martin Coplák <martin.coplak@gmail.com>
2018-06-04 14:19:55 +0200

    Modulized 2 types of sidebar

--- src/main/java/net/minecord/xoreboardutil/bukkit/XoreBoardPlayerSidebar.java
+++ src/main/java/net/minecord/xoreboardutil/bukkit/XoreBoardPlayerSidebar.java
@@ -1,8 +1,65 @@
 package net.minecord.xoreboardutil.bukkit;
 
 import lombok.Getter;
+import net.minecord.xoreboardutil.Sidebar;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 @Getter
-public class XoreBoardPlayerSidebar {
+public class XoreBoardPlayerSidebar implements Sidebar {
 
-}
+    @Override
+    public void setDisplayName(@NotNull String displayName) {
+
+    }
+
+    @Override
+    public void putLine(@NotNull String lineKey, int value) {
+
+    }
+
+    @Override
+    public void setLines(HashMap<String, Integer> lineKeys) {
+
+    }
+
+    @Override
+    public HashMap<String, Integer> getLines() {
+        return null;
+    }
+
+    @Override
+    public void rewriteLines(HashMap<String, Integer> lineKeys) {
+
+    }
+
+    @Override
+    public void clearLine(@NotNull String lineKey) {
+
+    }
+
+    @Override
+    public void clearLines() {
+
+    }
+
+    @Override
+    public void hideSidebar() {
+
+    }
+
+    @Override
+    public void showSidebar() {
+
+    }
+
+    @Override
+    public boolean isShowed() {
+        return false;
+    }
+
+    @Override
+    public void setShowedSidebar(boolean statement) {
+
+}}
