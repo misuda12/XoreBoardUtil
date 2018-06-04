c0ba0db09b9d29e94adec1523bdfd2a0ac85c5fc
Martin Coplák <martin.coplak@gmail.com>
2018-06-04 14:09:41 +0200

    Initial commit

---
+++ src/main/java/net/minecord/xoreboardutil/bukkit/Sidebar.java
@@ -0,0 +1,64 @@
+package net.minecord.xoreboardutil.bukkit;
+
+import lombok.Getter;
+
+@Getter
+public interface Sidebar {
+
+    /**
+     * void setDisplayName(@org.jetbrains.annotations.NotNull String displayName)
+     * @param displayName String {@link String}
+     */
+
+    void setDisplayName(@org.jetbrains.annotations.NotNull String displayName);
+
+    /**
+     * void putLine(@org.jetbrains.annotations.NotNull String lineKey, int value)
+     * @param lineKey String {@link String}
+     * @param value int {@link Integer}
+     */
+
+    void putLine(@org.jetbrains.annotations.NotNull String lineKey, int value);
+
+    /**
+     * void putLines(java.util.HashMap<String, Integer> lineKeys)
+     * @param lineKeys HashMap {@link java.util.HashMap}
+     */
+
+    void putLines(java.util.HashMap<String, Integer> lineKeys);
+
+    /**
+     * void rewriteLines(java.util.HashMap<String, Integer> lineKeys)
+     * @param lineKeys HashMap {@link java.util.HashMap}
+     */
+
+    void rewriteLines(java.util.HashMap<String, Integer> lineKeys);
+
+    /**
+     * void clearLine(@org.jetbrains.annotations.NotNull String lineKey)
+     * @param lineKey String {@link String}
+     */
+
+    void clearLine(@org.jetbrains.annotations.NotNull String lineKey);
+
+    void clearLines();
+
+    void hideSidebar();
+
+    void showSidebar();
+
+    /**
+     * boolean isSidebar()
+     * @return boolean
+     */
+
+    boolean isShowed();
+
+    /**
+     * void setShowedSidebar(boolean statement)
+     * @param statement boolean {@link Boolean}
+     */
+
+    void setShowedSidebar(boolean statement);
+
+}
--- src/main/java/net/minecord/xoreboardutil/bukkit/XoreBoardPlayerSidebar.java
+++ src/main/java/net/minecord/xoreboardutil/bukkit/XoreBoardPlayerSidebar.java
@@ -1,5 +1,8 @@
 package net.minecord.xoreboardutil.bukkit;
 
+import lombok.Getter;
+
+@Getter
 public class XoreBoardPlayerSidebar {
 
 }
