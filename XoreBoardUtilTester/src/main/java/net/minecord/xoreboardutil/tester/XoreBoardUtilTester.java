c5dfaa4cd992aa80290e6d390e19fdeeab452eff
misuda12 <martin.coplak@gmail.com>
2018-09-12 22:49:13 +0200

    Fixed imp error, edited configuration, created test module class, edited XoreBoard, updated maven & pom.xml

--- XoreBoardUtilTester/src/main/java/net/minecord/xoreboardutil/tester/XoreBoardUtilTester.java
+++ XoreBoardUtilTester/src/main/java/net/minecord/xoreboardutil/tester/XoreBoardUtilTester.java
@@ -1,17 +1,39 @@
 package net.minecord.xoreboardutil.tester;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import lombok.Getter;
+import net.minecord.xoreboardutil.bukkit.XoreBoard;
+import net.minecord.xoreboardutil.bukkit.XoreBoardUtil;
+import net.minecord.xoreboardutil.bukkit.event.XoreBoardSendPacketEvent;
+import org.jetbrains.annotations.NotNull;
 
-public final class XoreBoardUtilTester extends JavaPlugin {
+import java.util.HashMap;
+import java.util.logging.Level;
+
+@Getter
+public final class XoreBoardUtilTester extends org.bukkit.plugin.java.JavaPlugin implements org.bukkit.event.Listener {
+    private @NotNull XoreBoard xoreBoard;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        getServer().getPluginManager().registerEvents(this, this);
+
+        this.xoreBoard = XoreBoardUtil.getNextXoreBoard();
+        @NotNull String defaultTitle = this.xoreBoard.setDefaultTitle("<3 Unicorns, do we ?");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @org.bukkit.event.EventHandler
+    public void on(@NotNull final org.bukkit.event.player.PlayerJoinEvent event) {
+        this.xoreBoard.addPlayer(event.getPlayer());
+        @NotNull HashMap<String, Integer> lines = new HashMap<String, Integer>();
+        lines.put("packetID: " + this.xoreBoard.getPlayer(event.getPlayer()).getID(), 1);
+            lines.put("name: " + event.getPlayer().getName(), 2);
+                lines.put("pID: " + event.getPlayer().getEntityId(), 3);
+        this.xoreBoard.getSharedSidebar().putLines(lines);
+        this.xoreBoard.getSharedSidebar().showSidebar();
     }
-}
+
+    @org.bukkit.event.EventHandler
+    public void packet(@NotNull final XoreBoardSendPacketEvent event) {
+        getLogger().log(Level.CONFIG, event.getPacketObject().toString());
+}}
